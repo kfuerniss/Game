b98705f3ea01c337384a49e45900ea8f8178b7f1
kfuerniss <31049234+kfuerniss@users.noreply.github.com>
2017-12-19 18:56:41 -0800

    no message

--- Game/js/app.js
+++ Game/js/app.js
@@ -22,6 +22,7 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
@@ -29,9 +30,18 @@ Enemy.prototype.render = function() {
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [];
 // Place the player object in a variable called player
+var player = function(Y,X){
 
+	this.sprite = 'images/char-boy.png';
+	this.x = x;
+    this.y = Y;
+};
 
+player.prototype.update = function(dt){
+
+};
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
